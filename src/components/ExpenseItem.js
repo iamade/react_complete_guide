37fdfd6ed3b9ccc4b87b36eb39f3270104913b07
Adesegun Koiki <adesegun.koiki@sterling.ng>
2022-12-30 16:29:46 +0100

    props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,19 @@
- function ExpenseItem() {
+ import './ExpenseItem.css';
+ 
+ function ExpenseItem(props) {
+    // const expenseDate = new Date(2021, 2, 28);
+    // const expenseTitle = 'Car Insurance';
+    // const expenseAmount = 294.67;
+
     return (
-    <div>
-        <div>Date</div>
-        <div>
-            <h2>Title</h2>
-            <div>Amount</div>
+    <div className="expense-item">
+        <div>{props.date.toISOString()}</div>
+        <div className='expense-item__description'>
+            <h2>{props.title}</h2>
+            <div className='expense-item__price'>${props.amount}</div>
         </div>
     </div>
     );
  }
 
- export default ExpenseItem
+ export default ExpenseItem;
